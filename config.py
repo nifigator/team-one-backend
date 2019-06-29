bd6fa32614fa95ba447d55b81af28059a2be3cbb
Boris Pokhozhaev <nifigator@gmail.com>
2019-06-29 23:03:17 +0700

    Add SQLAwqlchemy

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 import connexion
 # import yaml
-# from flask_migrate import Migrate
-# from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 # from flask_marshmallow import Marshmallow
 
 from swagger_ui_bundle import swagger_ui_3_path
@@ -22,11 +29,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
   
 # config = get_config(config_file)
 
-# DB_USER = config['db']['user']
-# DB_PASSWORD = config['db'].get('password', '')
-# DB_NAME = config['db']['base']
-# DB_HOST = config['db'].get('host', 'localhost')
-# DB_PORT = config['db'].get('port', 5432)
+DB_USER = os.environ.get('ONE_DB_USER')
+DB_PASSWORD = os.environ.get('ONE_DB_PASSWORD')
+DB_NAME = os.environ.get('ONE_DB_NAME')
+DB_HOST = os.environ.get('ONE_DB_HOST', 'localhost')
+DB_PORT = os.environ.get('ONE_DB_PORT', 5432)
 
 # JWT_ISSUER = config['jwt']['issuer']
 # JWT_SECRET = config['jwt']['secret']
@@ -43,14 +50,15 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 # app.config['SQLALCHEMY_ECHO'] = config.get('sql_echo') or False
-# app.config['SQLALCHEMY_DATABASE_URI'] = \
-#     'postgresql://{user}:{password}@{host}:{port}/{base}'.format(user=DB_USER,
-#     password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, base=DB_NAME)
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql://{user}:{password}@{host}:{port}/{base}'.format(user=DB_USER,
+    password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, base=DB_NAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Initiialize Marshmallow
 # ma = Marshmallow(app)
