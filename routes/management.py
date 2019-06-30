a15eb5a6b4a0ba04eea708c65bd7aed400e0ed17
Boris Pokhozhaev <nifigator@gmail.com>
2019-06-30 10:34:24 +0700

    Add sizing to issues list

--- routes/management.py
+++ routes/management.py
@@ -1,7 +1,14 @@
 from models import db, Issue, IssueSchema
 
-def get_issues() -> tuple:
-    issues = Issue.query.order_by(Issue.id).all()
+def get_issues(offset=0, limit=None) -> tuple:
+    # issues = Issue.query.order_by(Issue.id).all()
+    issues = Issue.query
+    issues = issues.order_by(Issue.id)
+    if offset >= 0 and (limit == None or limit > 0):
+        limit = offset + limit if limit else None
+        issues = issues.slice(offset, limit)
+    else:
+        issues = issues.all()
     issues_schema = IssueSchema(many=True)
     issues_data = issues_schema.dump(issues).data
     return issues_data, 200
