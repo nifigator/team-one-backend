2c305cb49c6a72107e154f61e9cde99f41926e04
Boris Pokhozhaev <nifigator@gmail.com>
2019-06-30 13:03:54 +0700

    Add customer data in issues

--- routes/management.py
+++ routes/management.py
@@ -1,10 +1,23 @@
 from models import db, Issue, IssueSchema
 
+from routes.customers import customers
+
 def get_issues(offset=0, limit=None, sort='') -> tuple:
     # issues = Issue.query.order_by(Issue.id).all()
     issues = Issue.query
-    if sort:
-        sort_fields = sorf.split(',')
+    # if sort:
+    #     # sort_fields = sort.split(',')
+    #     if sort.startswith('-'):
+    #         print('Match desc')
+    #     field = sort
+    #     issue_schema = IssueSchema(many=True)
+    #     fields = issue_schema.declared_fields.keys()
+    #     if field in fields:
+    #         print('Field match')
+    #     # valid_sort_fields = list(set(fields) & set(sort_fields))
+    #     # for field in valid_sort_fields:
+    #     #     issues.order_by(field)
+    # else:
     issues = issues.order_by(Issue.id)
     if offset >= 0 and (limit == None or limit > 0):
         limit = offset + limit if limit else None
@@ -13,5 +26,12 @@ def get_issues(offset=0, limit=None, sort='') -> tuple:
         issues = issues.all()
     issues_schema = IssueSchema(many=True)
     issues_data = issues_schema.dump(issues).data
+    issues_with_custs = []
+    for issue in issues_data:
+        if issue.get('customer_id'):
+            customer = customers.get(issue.get('customer_id'))
+            if customer:
+                issue['customer'] = customer
+
     return issues_data, 200
 
